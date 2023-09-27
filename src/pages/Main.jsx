99437f4a2dd4178a8b090c2d72bc1d72d62e7107
Thiago Cavalcante <th-fernandes@hotmail.com>
2023-09-27 16:54:23 -0300

    TESTE

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -14,24 +14,12 @@ const Main = () => {
 
       <div className="content-main">
         <div className="text-content">
-          <h1>CONSTRUÇÕES E REFORMAS RESIDENCIAIS E INDUSTRIAIS</h1>
+          <h1>GALPÕES EM CONDOMINIO LOGÍSTICO</h1>
           
           <h3>Entre em contato e solicite o seu orçamento.</h3>
         </div>
 
-        <div className="form">
-        <form action="https://formsubmit.co/your@email.com" method='POST'>
-            <label htmlFor="">Nome:</label>
-            <input type="text" />
-            <label htmlFor="">Email:</label>
-            <input type="text" />
-            <label htmlFor="">Digite sua mensagem:</label>
-            <textarea name="" id="" cols="40" rows="5"></textarea>
-            <button type="submit">Enviar</button>
         
-        </form>
-          
-        </div>
 
         
 
